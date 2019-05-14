9a6723b860ce86da32dcaedbfa9d5b1d80fd6610
Laura Bodell <bodelllaura@gmail.com>
2019-05-14 19:44:19 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,11 +3,16 @@ import { connect } from 'react-redux'
 
 class Users extends Component {
 
-  render() {
+
+
+render() {
+    let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>);
+
     return (
       <div>
+        {this.props.numberOfUsers}
         <ul>
-          Users!
+          {users}
         </ul>
       </div>
     )
